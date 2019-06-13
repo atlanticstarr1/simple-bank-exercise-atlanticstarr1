aadd76e1fe65bdfdfbb1bfd3c2b88d71a295053e
DAVID NOEL <DAVID.NOEL@aexp.com>
2019-06-12 22:47:05 -0400

    added rhombus oracle

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 import 'openzeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import "../contracts/Ilighthouse.sol"; // Rhombus oracle
 
 /// @author David Noel
 /// @title A bank contract that pays its users interest on a daily basis.
@@ -12,6 +13,12 @@ contract SimpleBank is Ownable, Pausable {
     //
     // State variables
     //
+    // Interest rate Oracle
+    ILighthouse  public myLighthouse;
+    // Array of users registered
+    address[] private accounts;
+    // Holds number of registered users
+    uint private numAccounts = 0;
 
     /* Protect our users balance from being viewable from other contracts. */
     mapping (address => uint) private balances;
@@ -27,10 +34,13 @@ contract SimpleBank is Ownable, Pausable {
     event Enrolled(address indexed accountAddress);
 
     /* When a user makes a deposit */
-    event Deposited(address indexed accountAddress, uint amount);
+    event Deposited(address indexed accountAddress, uint amount, uint newBalance);
 
     /* When a user makes a withdrawal */
     event Withdrawn(address indexed accountAddress, uint withdrawAmount, uint newBalance);
+
+    /* When a user closes their account */
+    event Closed(address indexed accountAddress, uint balanceAtClose);
 
     //
     // Modifiers
@@ -53,16 +63,76 @@ contract SimpleBank is Ownable, Pausable {
         _;
     }
 
+    constructor(ILighthouse _myLighthouse) public {
+        myLighthouse = _myLighthouse;
+    }
+
     //
     // Functions
     //
 
+    function getInterestRate() public view returns(uint){
+        uint interestRate;
+        bool ok;
+        (interestRate,ok) = myLighthouse.peekData(); // obtain random number from Rhombus Lighthouse
+        return interestRate;
+    }
+
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
-    function enroll() public checkEnroll() whenNotPaused() returns (bool _enrolled){
+    function enroll() public checkEnroll whenNotPaused returns (bool _enrolled){
         enrolled[msg.sender] = true;
+        accounts.push(msg.sender);
         _enrolled = enrolled[msg.sender];
         emit Enrolled(msg.sender);
+    }
+
+    /// @notice Deposit ether into bank
+    /// @return The balance of the user after the deposit is made
+    // Users should be enrolled before they can make deposits
+    function deposit() external payable isEnrolled whenNotPaused returns (uint _balance) {
+        require(msg.value > 0, "Deposit must be greater than 0");
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        _balance = balances[msg.sender];
+        emit Deposited(msg.sender, msg.value, _balance);
+    }
+
+    /// @notice Withdraw ether from bank
+    /// @dev This does not return any excess ether sent to it
+    /// @param withdrawAmount amount you want to withdraw
+    /// @return The balance remaining for the user
+    // checks-effects-interaction pattern to prevent re-entrancy
+    function withdraw(uint withdrawAmount) external isEnrolled checkWithdraw(withdrawAmount) returns (uint _balance) {
+        balances[msg.sender] = balances[msg.sender].sub(withdrawAmount);
+        msg.sender.transfer(withdrawAmount);
+        _balance = balances[msg.sender];
+        emit Withdrawn(msg.sender, withdrawAmount, _balance);
+    }
+
+    /// @notice Close bank account and return all user's balance to them.
+    /// @return balance of user
+    // checks-effects-interaction pattern to prevent re-entrancy
+    function closeAccount() external isEnrolled returns (bool) {
+        uint totalBalance = balances[msg.sender];
+        balances[msg.sender] = 0;
+        enrolled[msg.sender] = false;
+        if(totalBalance > 0){
+            msg.sender.transfer(totalBalance);
+        }
+        emit Closed(msg.sender, totalBalance);
+        return !enrolled[msg.sender];
+    }
+
+    /// @notice Get all registered accounts
+    /// @return Array of accounts
+    function getAccounts() public view onlyOwner returns (address[] memory) {
+        return accounts;
+    }
+
+    /// @notice Get number of registered accounts.
+    /// @return Number of accounts.
+    function getNumberOfAccounts() public view onlyOwner returns (uint) {
+        return accounts.length;
     }
 
     /// @notice Get balance
@@ -73,29 +143,8 @@ contract SimpleBank is Ownable, Pausable {
 
     /// @notice Get balance of contract
     /// @return The balance of the contract
-    function getContractBalance() public view returns(uint) {
+    function getContractBalance() public view onlyOwner returns(uint) {
         return address(this).balance;
-    }
-
-    /// @notice Deposit ether into bank
-    /// @return The balance of the user after the deposit is made
-    // Users should be enrolled before they can make deposits
-    function deposit() external payable isEnrolled() whenNotPaused() returns (uint _balance) {
-        require(msg.value > 0, "Deposit must be greater than 0");
-        balances[msg.sender] = balances[msg.sender].add(msg.value);
-        _balance = balances[msg.sender];
-        emit Deposited(msg.sender, msg.value);
-    }
-
-    /// @notice Withdraw ether from bank
-    /// @dev This does not return any excess ether sent to it
-    /// @param withdrawAmount amount you want to withdraw
-    /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public isEnrolled() checkWithdraw(withdrawAmount) returns (uint _balance) {
-        balances[msg.sender] = balances[msg.sender].sub(withdrawAmount);
-        msg.sender.transfer(withdrawAmount);
-        _balance = balances[msg.sender];
-        emit Withdrawn(msg.sender, withdrawAmount, _balance);
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -103,7 +152,9 @@ contract SimpleBank is Ownable, Pausable {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() external {
-        revert("Reverting contract state.");
+    function() external payable {
+        // require(msg.data.length == 0,"no data sent with message");
+        // emit Deposited
+        revert();
     }
 }
--- truffle.js
+++ truffle.js
@@ -1,12 +1,17 @@
-module.exports = {
-//   compilers: {
-//     solc: '0.4.25'
-//   },
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
+import HDWalletProvider from "truffle-hdwallet-provider";
+
+const mnemonic = ""; // Enter the mnemonic for your rinkeby account (testnet deployment only)
+
+export const networks = {
+  development: {
+    host: "localhost",
+    port: 8545,
+    network_id: "*" // Match any network id
+  },
+  rinkeby: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/[your infura link here]");
+    },
+    network_id: 4
   }
 };
